26ae141de29dd951bba683450a8c3fab09d49afe
Documenter.jl <documenter@juliadocs.github.io>
2025-05-12 14:50:09 +0000

    build based on ceda9f3

---
+++ v0.0.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#ProtectedArrays","page":"Home","title":"ProtectedArrays","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ProtectedArrays.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
